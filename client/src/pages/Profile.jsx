e2e9fc67e8a1f789d2d14064f9733c6b320379f8
vishxlkr <vishxlkr@gmail.com>
2025-08-29 18:39:31 +0530

    update

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,6 +9,7 @@ import Loading from "../components/Loading";
 import UserProfileInfo from "../components/UserProfileInfo";
 import PostCard from "../components/PostCard";
 import moment from "moment";
+import ProfileModal from "../components/ProfileModal";
 
 const Profile = () => {
    const { profileId } = useParams();
@@ -109,7 +110,7 @@ const Profile = () => {
             )}
          </div>
          {/* edit profile modal */}
-         {showEdit && <p>show profile edit</p>}
+         {showEdit && <ProfileModal setShowEdit={setShowEdit} />}
       </div>
    ) : (
       <Loading />
