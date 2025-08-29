c60555280d2f7411109b3ba3d767d9838ce4614f
vishxlkr <vishxlkr@gmail.com>
2025-08-29 18:43:06 +0530

    update

--- client/src/components/ProfileModal.jsx
+++ client/src/components/ProfileModal.jsx
@@ -170,6 +170,17 @@ const ProfileModal = () => {
                         value={editForm.location}
                      />
                   </div>
+                  <div className="flex justify-end space-x-3 pt-6">
+                     <button className="px-4 py-2 border border-gray-300 rounded-lg text-gray-700 hover:bg-gray-50 transition-colors">
+                        Cancel
+                     </button>
+                     <button
+                        type="submit"
+                        className="px-4 py2 bg-gradient-to-r from-indigo-500 to-purple-600 text-white rounded-lg hover:from-indigo-600  hover:to-purple-700 transition cursor-pointer"
+                     >
+                        Save Changes
+                     </button>
+                  </div>
                </form>
             </div>
          </div>
