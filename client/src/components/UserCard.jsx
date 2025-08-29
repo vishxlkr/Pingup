2f10584bdb10bd7396a80ec2beb84fa0ff73247d
vishxlkr <vishxlkr@gmail.com>
2025-08-29 13:33:50 +0530

    update

--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { dummyConnectionsData, dummyUserData } from "../assets/assets";
-import { MapPin } from "lucide-react";
+import { MapPin, MessageCircle, Plus, UserPlus } from "lucide-react";
 
 const UserCard = ({ user }) => {
    const currentUser = dummyUserData;
@@ -29,7 +29,6 @@ const UserCard = ({ user }) => {
                </p>
             )}
          </div>
-
          <div className="flex items-center justify-center gap-2 mt-4 text-xs text-gray-600">
             <div className="flex items-center gap-1 border border-gray-300 rounded-full px-3 py-1">
                <MapPin className="w-4 h-4" />
@@ -38,6 +37,30 @@ const UserCard = ({ user }) => {
             <div className="flex items-center gap-1 border border-gray-300 rounded-full px-3 py-1">
                <span>{user.followers.length}</span> Followers
             </div>
+         </div>
+         <div className="flex mt-4 gap-2">
+            {/* follow button */}
+            <button
+               onClick={handleFollow}
+               disabled={currentUser?.following.includes(user._id)}
+               className="w-full py-2 rounded-md flex justify-center items-center gap-2 bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 active:sc95 transition text-white cursor-pointer"
+            >
+               <UserPlus className="w-4 h-4" />
+               {currentUser?.following.includes(user._id)
+                  ? "Following"
+                  : "Follow"}
+            </button>
+            {/* connnection request button / message button */}
+            <button
+               onClick={handleConnectionRequest}
+               className="flex items-center justify-center w-16 border text-slate-500 group rounded-md cursor-pointer active:scale-95 transition"
+            >
+               {currentUser?.connections.includes(user._id) ? (
+                  <MessageCircle className="w-5 h-5 group-hover:scale-120 transition " />
+               ) : (
+                  <Plus className="w-5 h-5 group-hover:scale-150 transition" />
+               )}
+            </button>
          </div>
       </div>
    );
